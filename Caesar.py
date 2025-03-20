6b8cabf1ac8891760da08cbcb26a45af55874ed0
kamalGoku <131174339+kamalGoku@users.noreply.github.com>
2025-03-20 22:40:47 +0530

    Create Caesar.py

---
+++ Caesar.py
@@ -0,0 +1,11 @@
+'''
+https://leetcode.com/discuss/post/395045/facebook-phone-screen-caesar-cipher-by-a-85v6/
+Caesar Cipher Encrpytion
+You are given a list of string, group them if they are same after using Ceaser Cipher Encrpytion.
+Definition of "same", "abc" can right shift 1, get "bcd", here you can shift as many time as you want, the string will be considered as same.
+
+Example:
+
+Input: ["abc", "bcd", "acd", "dfg"]
+Output: [["abc", "bcd"], ["acd", "dfg"]]
+'''
