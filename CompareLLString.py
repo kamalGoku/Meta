8399a8515665f71b27e33d2bad6ca164a515b6e4
kamalGoku <131174339+kamalGoku@users.noreply.github.com>
2025-03-20 22:01:00 +0530

    Create CompareLLString.py

---
+++ CompareLLString.py
@@ -0,0 +1,6 @@
+'''
+Compare strings stored in two Linked Lists . Return true if the strings stored in both the lists are simillar.
+
+List 1 : "He" -> " llo" -> "wor" -> "ld"
+List 2 : "H" --> "e" --> "ll" --> "owo" --> "r" --> "ld"
+'''
