0d0f9639cf934d62253756c632470a2ca941b011
kamalGoku <131174339+kamalGoku@users.noreply.github.com>
2025-03-20 22:49:15 +0530

    Create RoundTrip.py

---
+++ RoundTrip.py
@@ -0,0 +1,8 @@
+'''
+Plan a round trip between 2 cities with minimum flight cost Given 2 arrays - Deprature costs and Return Costs.
+Return minCost and the indexes of both the array
+Both arrays will always have same size.
+You can take a flight on the same day
+It cannot be 7+7 as you can't take return flight without taking departure flight.
+D =[10,8,9,11,7] R =[8,8,10,7,9] ; Ans = 15 (8+7) ; arr[]=[1,3]
+'''
