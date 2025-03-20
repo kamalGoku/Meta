9d2fd7c4ff2b8914d93802c90e27267ab81aff3d
kamalGoku <131174339+kamalGoku@users.noreply.github.com>
2025-03-20 22:02:51 +0530

    Create UserScores.py

---
+++ UserScores.py
@@ -0,0 +1,4 @@
+'''
+Given a list of user scores as an input, return the index of any player who has the highest score at random
+Ex: Input: [1,3,4,2,1,4,3,4] Output: 2, 5, or 7 with even chance
+'''
