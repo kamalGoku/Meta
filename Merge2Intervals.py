408cc279e358d2824c37dc4f920626dd2db3e745
kamalGoku <131174339+kamalGoku@users.noreply.github.com>
2025-03-20 22:39:56 +0530

    Create Merge2Intervals.py

---
+++ Merge2Intervals.py
@@ -0,0 +1,11 @@
+'''
+Given A and B two interval lists, A has no overlap inside A and B has no overlap inside B. Write the function to merge two interval lists, output the result with no overlap. Ask for a very efficient solution
+
+A naive method can combine the two list, and sort and apply merge interval in the leetcode, but is not efficient enough.
+
+For example,
+A: [1,5], [10,14], [16,18]
+B: [2,6], [8,10], [11,20]
+
+output [1,6], [8, 20]
+'''
