60d51ebb43f38f6f598e72de81ca24ca43acf655
kamalGoku <131174339+kamalGoku@users.noreply.github.com>
2025-03-20 22:51:53 +0530

    Create RecentElement.py

---
+++ RecentElement.py
@@ -0,0 +1,9 @@
+'''
+Implement the following interfaces for a class
+int get(string key) -> get the value for a key
+void Put(string key, itn val) -> inserts the key and corresponding value with the key.
+void Delete(string key) -> Deletes an existing key
+int Last() -> return the most recently accessed non deleted key.
+
+all of these should run in O(n) time.
+'''
