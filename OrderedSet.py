8c1594e6085dcbc3c3f07cc737e37921d1288519
kamalGoku <131174339+kamalGoku@users.noreply.github.com>
2025-03-22 12:42:45 +0530

    Update OrderedSet.py

--- OrderedSet.py
+++ OrderedSet.py
@@ -4,4 +4,10 @@ Implement OrderedSet
 .remove(num)
 .toArray()
 .contain(num)
+
+Design a set with the below functionalities
+add(element) - increments value for element. If not present, add element with value 1.
+remove(element) - decrements value for element. If value reaches 0, remove the element. (Was asked to decide what would be a better option in case key was not present)
+count(element) - returns count
+
 '''
