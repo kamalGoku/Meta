30f7c19e072bb2594fff96851b948fe291080ed9
kamalGoku <131174339+kamalGoku@users.noreply.github.com>
2025-03-31 18:28:40 +0530

    Update Merge3Lists.py

--- Merge3Lists.py
+++ Merge3Lists.py
@@ -32,7 +32,28 @@ def mergeLists(listA, listB, listC):
     return res
 
 
+def mergeListsWithoutDuplicates(listA, listB, listC):
+    res = []
+    i=0
+    j=0
+    k=0
+    while i<len(listA) or j<len(listB) or k<len(listC):
+        aVal = listA[i] if i<len(listA) else sys.maxsize
+        bVal = listB[j] if j<len(listB) else sys.maxsize
+        cVal = listC[k] if k<len(listC) else sys.maxsize
+        minVal = min(aVal, bVal, cVal)
+        if not res or res[-1]!=minVal: #Remove Duplicates
+            res.append(minVal)
+        if minVal==aVal:
+            i+=1
+        elif minVal==bVal:
+            j+=1
+        else:
+            k+=1
+    return res
+
 listA = [1,1,2,2,3,4]
 listB = [3,3,4,5,6]
 listC = [2,3,7,8,9,10]
 print(mergeLists(listA, listB, listC))
+print(mergeListsWithoutDuplicates(listA, listB, listC))
