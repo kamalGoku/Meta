60dbe131e9a824d4051d69b66f44621c0e8cadb9
kamalGoku <131174339+kamalGoku@users.noreply.github.com>
2025-04-07 13:08:27 +0530

    Update MouseCheese.py

--- MouseCheese.py
+++ MouseCheese.py
@@ -21,3 +21,47 @@ class Mouse {
 	}
 }
 '''
+'''
+Mouse and Cheese
+'''
+class Mouse:
+    def __init__(self):
+        self.directions = [(-1,0), #up
+                      (0,1), #right
+                      (1,0), #down
+                      (0,-1) #left
+                     ]
+        self.oppDirections = [(1,0), #down
+                              (0,-1), #left
+                              (-1,0), #up
+                              (0,1), #right
+                             ]
+        self.visited = set()
+
+    def move(self,direction):
+        pass
+
+    def hasCheese(self):
+        pass
+
+    def __dfs(self,row, col):
+        if self.hasCheese():
+            return True
+        self.visited.add((row, col))
+        for i in range(4):
+            dir = (dir+1)%4
+            newRow = row+self.directions[dir][0]
+            newCol = col+self.directions[dir][1]
+            if (row, col) in self.visited:
+                continue
+            if not self.move(self.directions[i]):
+                self.move(self.oppDirections[i])
+                continue
+
+            if self.__dfs(newRow, newCol):
+                return True
+
+            self.move(self.oppDirections[i])
+        return False
+    def getCheese(self):
+        self.__dfs(0,0)
